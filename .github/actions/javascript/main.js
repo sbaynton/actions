92176fb6ff75b24f8e40719fb7edd3bb14e0f3c7
sbaynton <spencer.baynton@system1.com>
2024-05-14 11:34:34 -0400

    Export all vars as env variables

--- .github/actions/javascript/main.js
+++ .github/actions/javascript/main.js
@@ -1,8 +1,11 @@
 const core = require('@actions/core');
 
 try {
-  const vars = core.getInput('vars', { required: true });
-  console.log(vars);
+  const json = core.getInput('vars', { required: true });
+  const vars = JSON.parse(json);
+  for (const [key, value] of Object.entries(vars)) {
+    core.exportVariable(key, value);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
